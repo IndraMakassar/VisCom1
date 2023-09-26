3a20b8fa6ad27a59ed154a9878f5d64ae854ab63
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-09-26 19:58:36 +0800

    add skeleton to main

--- bin/week_1.dart
+++ bin/week_1.dart
@@ -1,3 +1,37 @@
 
+import 'dart:io';
+
+import 'Expense.dart';
+
 void main(List<String> arguments) {
+  final List<Expense> expenses = [];
+
+  while(!true==false) {
+    print("===== expenses tracking =====");
+    print("1. Add expense");
+    print("2. Show expense history");
+    print("3. Show total expenses");
+    print("4. exit");
+    print("\nchoose a number:");
+
+    String? input = stdin.readLineSync();
+
+    switch (input) {
+      case "1" : {
+        addExpense();
+      } break;
+      case "2" : {
+        showExpenseHistory();
+      } break;
+      case "3" : {
+        showTotalExpense();
+      } break;
+      case "4" : {
+        exit(0);
+      }
+    }
+
+
+
+  }
 }
