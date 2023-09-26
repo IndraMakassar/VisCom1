d393bdab39393c8fb98e21df83142d5aed6adb5c
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-09-26 19:53:18 +0800

    add date in expense

--- bin/Expense.dart
+++ bin/Expense.dart
@@ -2,7 +2,8 @@ class Expense{
   final double amount;
   final String category;
   final String? note;
+  final DateTime date;
 
-  Expense(this.amount, this.category, this.note);
+  Expense(this.amount, this.category, this.note, this.date);
 
 }
