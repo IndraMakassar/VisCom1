27f8599b3cfc485e4d0344fb2e044751fa6e44d0
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-09-27 20:07:18 +0800

    add Expense.add to add expense with input

--- bin/Expense.dart
+++ bin/Expense.dart
@@ -1,13 +1,26 @@
+import 'dart:io';
+
 class Expense{
-  final double amount;
-  final String category;
-  final String? note;
-  final DateTime date;
+  late double amount;
+  late String category;
+  String? note;
+  late DateTime date;
 
   Expense(this.amount, this.category, this.note, this.date);
 
   @override
   String toString() {
-    return ("\$ $amount, $category, $note, $date")
+    return ("\$ $amount, $category, $note, $date");
+  }
+
+  Expense.add(){
+    print("Please enter the date: (YYYY-MM-DD)");
+    date = DateTime.parse(stdin.readLineSync()!);
+    print('Please enter the amount:');
+    amount = double.parse(stdin.readLineSync()!);
+    print("Please enter the category:");
+    category = stdin.readLineSync()!;
+    print("Please enter a note (opsional):");
+    note = stdin.readLineSync();
   }
 }
