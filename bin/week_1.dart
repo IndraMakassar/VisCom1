cdde70820d2a3deedd075f6ff5f4ef8494df1033
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-09-28 12:36:53 +0800

    applied all method

--- bin/week_1.dart
+++ bin/week_1.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'Expense.dart';
@@ -6,7 +5,7 @@ import 'Expense.dart';
 void main(List<String> arguments) {
   final List<Expense> expenses = [];
 
-  while(!true==false) {
+  while (!true == false) {
     print("===== expenses tracking =====");
     print("1. Add expense");
     print("2. Show expense history");
@@ -17,21 +16,42 @@ void main(List<String> arguments) {
     String? input = stdin.readLineSync();
 
     switch (input) {
-      case "1" : {
-        addExpense();
-      } break;
-      case "2" : {
-        showExpenseHistory();
-      } break;
-      case "3" : {
-        showTotalExpense();
-      } break;
-      case "4" : {
+      case "1":
+        addExpense(expenses, Expense.add());
+        break;
+      case "2":
+        showExpenseHistory(expenses);
+        break;
+      case "3":
+        print("input the month: (YYYY-MM)");
+        DateTime date = DateTime.parse(stdin.readLineSync()!);
+        showTotalExpenseInMonth(expenses, date);
+        break;
+      case "4":
         exit(0);
-      }
     }
-
-
-
   }
+}
+
+List<Expense> addExpense(List<Expense> expenses, Expense expense) {
+  expenses.add(expense);
+  return expenses;
+}
+
+void showExpenseHistory(List<Expense> expenses) {
+  expenses.forEach((element) {
+    print(element.toString());
+  });
+}
+
+void showTotalExpenseInMonth(List<Expense> expenses, DateTime yearMonth) {
+  double total = 0;
+  expenses
+      .where((element) =>
+          element.date.year + element.date.month ==
+          yearMonth.year + yearMonth.month)
+      .forEach((element) {
+    total += element.amount;
+  });
+  print('total amount = \$$total');
 }
