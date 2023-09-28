0a2a691a5c11b47ea52e8001af49a81a9c655e6d
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-09-28 11:39:15 +0800

    fragment input to have null check and empty check

--- bin/Expense.dart
+++ bin/Expense.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-class Expense{
+class Expense {
   late double amount;
   late String category;
   String? note;
@@ -10,16 +10,45 @@ class Expense{
 
   @override
   String toString() {
-    return ("\$ $amount, $category, $note, $date");
+    return ("\$$amount, $category, $note, $date");
   }
 
-  Expense.add(){
+  Expense.add() {
+    inputDate();
+    inputAmount();
+    inputCategory();
+    inputNote();
+  }
+
+  inputDate() {
     print("Please enter the date: (YYYY-MM-DD)");
-    date = DateTime.parse(stdin.readLineSync()!);
+    try {
+      date = DateTime.parse(stdin.readLineSync()!);
+    } on FormatException {
+      print("Please enter a valid date");
+      inputDate();
+    }
+  }
+
+  inputAmount() {
     print('Please enter the amount:');
-    amount = double.parse(stdin.readLineSync()!);
+    try {
+      amount = double.parse(stdin.readLineSync()!);
+    } catch (e) {
+      print('the amount is not valid');
+      inputAmount();
+    }
+  }
+
+  inputCategory() {
     print("Please enter the category:");
     category = stdin.readLineSync()!;
+    if (category.isEmpty) {
+      inputCategory();
+    }
+  }
+
+  inputNote() {
     print("Please enter a note (optional):");
     note = stdin.readLineSync();
   }
