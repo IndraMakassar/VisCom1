8c26c41970897702840005ea7cc6524c4c864a70
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-09-28 12:36:32 +0800

    add return and move print to expense.add

--- bin/Expense.dart
+++ bin/Expense.dart
@@ -15,42 +15,46 @@ class Expense {
   }
 
   Expense.add() {
+    print("Please enter the date: (YYYY-MM-DD)");
     inputDate();
+    print('Please enter the amount:');
     inputAmount();
+    print("Please enter the category:");
     inputCategory();
+    print("Please enter a note (optional):");
     inputNote();
   }
 
   inputDate() {
-    print("Please enter the date: (YYYY-MM-DD)");
     try {
       date = DateTime.parse(stdin.readLineSync()!);
     } on FormatException {
       print("Please enter a valid date");
       inputDate();
     }
+    return date;
   }
 
   inputAmount() {
-    print('Please enter the amount:');
     try {
       amount = double.parse(stdin.readLineSync()!);
     } catch (e) {
       print('the amount is not valid');
       inputAmount();
     }
+    return amount;
   }
 
   inputCategory() {
-    print("Please enter the category:");
     category = stdin.readLineSync()!;
     if (category.isEmpty) {
       inputCategory();
     }
+    return category;
   }
 
   inputNote() {
-    print("Please enter a note (optional):");
     note = stdin.readLineSync();
+    return note;
   }
 }
